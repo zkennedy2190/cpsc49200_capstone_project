c6ab48463c03727aeb256cde90e089c14257de05
zkennedy2190 <zachariahckennedy@lewisu.edu>
2025-08-17 17:46:11 -0500

    Initial Framework

---
+++ react_project/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, World!</h1>
+      <p>Welcome to my first React application.</p>
+    </div>
+  );
+}
+
+
+export default App;
