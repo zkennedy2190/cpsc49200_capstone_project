58289e9d151987d3600a7e1ca8655c4980c9d402
zkennedy2190 <zachariahckennedy@lewisu.edu>
2025-08-18 02:57:56 -0500

    Add parent & guardian dashboards

---
+++ react_project/src/pages/GuardianDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function GuardianDashboard() {
+  return (
+    <div>
+      <h2>Guardian Dashboard</h2>
+      <p>Coming soon!</p>
+    </div>
+  );
+}
+
+export default GuardianDashboard;
---
+++ react_project/src/pages/ParentDashboard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ParentDashboard() {
+  return (
+    <div>
+      <h2>Parent Dashboard</h2>
+      <p>Coming soon!</p>
+    </div>
+  );
+}
+
+export default ParentDashboard;
