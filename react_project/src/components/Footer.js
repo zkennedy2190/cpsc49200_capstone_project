8df012aa02d22c6f0bf2cbd1f8faed92202c3ea2
zkennedy2190 <zachariahckennedy@lewisu.edu>
2025-08-17 20:59:26 -0500

    Update Header/Footer.js for css class names

---
+++ react_project/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <Footer className="app-footer">
+            <h1>Aunt Mary’s Storybook Portal</h1>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ react_project/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header className="app-header">
+            <h1>Aunt Mary’s Storybook Portal</h1>
+        </header>
+    );
+}
+
+export default Header;
