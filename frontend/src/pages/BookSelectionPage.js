74dd04b94fe081be5c2dd5063e9595f42f06b6e0
zkennedy2190 <zachariahckennedy@lewisu.edu>
2025-08-18 17:26:32 -0500

    Tested & fixed errors

--- frontend/src/pages/BookSelectionPage.js
+++ frontend/src/pages/BookSelectionPage.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Paper from '@mui/material/Paper';
-import Table from '@mui/material.Table';
+import Table from '@mui/material/Table';
 import TableHead from '@mui/material/TableHead';
-import TableBody from '@mui/material.TableBody';
-import TableRow from '@mui/material.TableRow';
-import TableCell from '@mui.material.TableCell';
+import TableBody from '@mui/material/TableBody';
+import TableRow from '@mui/material/TableRow';
+import TableCell from '@mui.material/TableCell';
 
 const books = [
     { title: 'Goodnight Moon', author: 'Margaret Wise Brown' },
--- frontend/src/pages/SchedulePage.js
+++ frontend/src/pages/SchedulePage.js
@@ -4,9 +4,9 @@ import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Table from '@mui/material/Table';
 import TableHead from '@mui/material/TableHead';
-import TableBody from '@mui/material.TableBody';
+import TableBody from '@mui/material/TableBody';
 import TableRow from '@mui/material/TableRow';
-import TableCell from '@mui/material.TableCell';
+import TableCell from '@mui/material/TableCell';
 import Paper from '@mui/material/Paper';
 
 function SchedulePage() {
